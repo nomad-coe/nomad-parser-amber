6f5cedbdd626bea773f7886c171bb6baa095263b
Berk Onat <b.onat@warwick.ac.uk>
2017-07-20 16:27:56 +0100

    Corrected Scale typo in tests

--- src/test/scala/eu/nomad_lab/parsers/AmberParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/AmberParserSpec.scala
@@ -2,7 +2,7 @@ package eu.nomad_lab.parsers
 
 import org.specs2.mutable.Specification
 
-object AmberParserTests extends Specification {
+object AmberParserSpec extends Specification {
   "AmberParserTest" >> {
     "test with json-events" >> {
       ParserRun.parse(AmberParser, "parsers/amber/test/examples/03_Prod.out", "json-events") must_== ParseResult.ParseSuccess
