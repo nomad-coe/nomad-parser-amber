64e304d36849dce072b3b2f339309438eab65dd5
Berk Onat <b.onat@warwick.ac.uk>
2017-07-28 15:23:27 +0100

    Fixed typo in test file names

--- src/test/scala/eu/nomad_lab/parsers/AmberParserSpec.scala
+++ src/test/scala/eu/nomad_lab/parsers/AmberParserSpec.scala
@@ -8,7 +8,7 @@ object AmberParserSpec extends Specification {
       ParserRun.parse(AmberParser, "parsers/amber/test/examples/03_Prod.out", "json-events") must_== ParseResult.ParseSuccess
     }
     "test with json" >> {
-      ParserRun.parse(AmberParser, "parsers/amber/test/examples/03_prod.out", "json") must_== ParseResult.ParseSuccess
+      ParserRun.parse(AmberParser, "parsers/amber/test/examples/03_Prod.out", "json") must_== ParseResult.ParseSuccess
     }
   }
 }
